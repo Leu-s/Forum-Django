f23270334fdaf367240a99f4835988127df536b8
unknown <leusnazarii@gmail.com>
2021-02-23 18:54:23 +0200

    Changed the algorithm for password validation during registration

--- main/forms.py
+++ main/forms.py
@@ -32,28 +32,25 @@ class UserRegistrationForm(forms.ModelForm):
             print(f'Error on form: {error}')
         super().add_error(field, error)
 
-    # def clean_password1(self):
-    #     password1 = self.cleaned_data['password1']
-    #     if password1:
-    #         password_validation.validate_password(password=password1)
-    #     !!!Не возбуждается ValidationError() если пароль не прошел валидацию
-    #     return password1
-
-    def clean(self):
-        super().clean()
+    def clean_password2(self):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
-        if password1:
-            password_validation.validate_password(password=password1)
         if password1 and password2 \
                 and password1 != password2:
-            errors = {
-                'password2': ValidationError(
-                    'Паролі не співпали',
-                    code='password_mismatch'
-                )
-            }
-            raise ValidationError(errors)
+            raise ValidationError(
+                'Паролі не співпали',
+                code='password_mismatch'
+            )
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        password = self.cleaned_data['password1']
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error('password1', error)
 
     def save(self, commit=True):
         user = super().save(commit=False)
